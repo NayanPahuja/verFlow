a0de3b11fb3ba519bbf1af2da7d672757280e75e
Nayan Pahuja <nayanpahuja25@gmail.com>
2024-06-01 11:52:22 +0530

    Update to repoFile and code Structure

--- libVerFlow.py
+++ libVerFlow.py
@@ -16,6 +16,7 @@ import hashlib
 from math import ceil
 import re
 import zlib
+import verFlowRepository
 
 #importing other files
 
@@ -32,100 +33,6 @@ argsp.add_argument("path",
                    default=".",
                    help="Where to create the repository"
                    )
-
-import os
-import configparser
-
-
-class VerFlowRepository(object):
-    """A git repository"""
-
-    # initializing the git repository
-    worktree = None
-    gitDir = None
-    conf = None
-
-    # utility function for repository path
-    def repoPath(self, *path):
-        return os.path.join(self.gitDir, *path)
-
-    def repoFile(self, *path, mkdir=False):
-        if self.repoDir(*path[:-1], mkdir=mkdir):
-            return self.repoPath(*path)
-
-    def repoDir(self, *path, mkdir=False):
-        path = self.repoPath(*path)
-
-        if os.path.exists(path):
-            if os.path.isdir(path):
-                return path
-            else:
-                raise Exception("Not a directory %s" % path)
-
-        if mkdir:
-            os.makedirs(path)
-            return path
-        else:
-            return None
-
-    def __init__(self, path, force=False):
-        self.worktree = path
-        self.gitDir = os.path.join(path, '.git')
-
-        # checking if the path is a git repository
-        if not (force or os.path.isdir(self.gitDir)):
-            raise Exception("Not a VerFlow repository %s" % path)
-
-        # reading the configuration file
-        self.conf = configparser.ConfigParser()
-        cf = self.repoFile('config')
-
-        if cf and os.path.exists(cf):
-            self.conf.read([cf])
-        elif not force:
-            raise Exception("Configuration file missing")
-
-        if not force:
-            vers = int(self.conf.get('core', 'repositoryformatversion'))
-            if vers != 0:
-                raise Exception("Unsupported repositoryformatversion %s" % vers)
-
-    def repoCreate(self,path):
-        repo = VerFlowRepository(path, True)  # repository object
-        # validating the directory
-        if os.path.exists(repo.worktree):
-            if not os.path.isdir(repo.worktree):
-                raise Exception("%s is not a directory!" % path)
-            if os.path.exists(repo.gitDir) and os.listdir(repo.gitDir):
-                raise Exception("%s Already contains a git repository!" % path)
-        else:
-            os.makedirs(repo.worktree)
-
-        assert repo.repoDir("branches", mkdir=True)
-        assert repo.repoDir("objects",mkdir=True)
-    
-        #.git/description
-        with open(repo.repoFile("description"),"w") as f:
-            f.write("Unnamed Repository. Edit this file 'description' to name the repository. \n")
-
-        #.git/HEAD
-        with open(repo.repoFile("HEAD"),"w") as f:
-            f.write("ref: refs/heads/master\n")
-        
-        with open(repo.repoFile("config"), "w") as f:
-            config = repo.repoDefaultConfig()
-            config.write(f)
-
-        return repo
-    def repoDefaultConfig(self):
-        conf = configparser.ConfigParser()
-
-        conf.add_section("core")
-        conf.set("core","repositoryFormatVersion" , "0")
-        conf.set("core", "filemode","false")
-        conf.set("core","bare","false")
-
-        return conf
 
 def main(argv = sys.argv[1:]):
     args = argparser.parse_args(argv)
@@ -167,4 +74,4 @@ def main(argv = sys.argv[1:]):
 
 #cmd_init handler
 def cmd_init(args):
-    VerFlowRepository.repoCreate(args.path)
+    verFlowRepository.repoCreate(args.path)
---
+++ verFlowRepository.py
@@ -0,0 +1,103 @@
+import configparser
+import os
+
+class verFlowRepository(object):
+  """A git repository"""
+
+  worktree = None
+  gitdir = None
+  conf = None
+
+  def __init__(self, path, force=False):
+    self.worktree = path
+    self.gitdir = os.path.join(path, ".ver_flow")
+
+    if not (force or os.path.isdir(self.gitdir)):
+      raise Exception("Not a verFlow repository %s" % path)
+
+    # Read configuration file in .git/config
+    self.conf = configparser.ConfigParser()
+    cf = repoFile(self, "config")
+
+    if cf and os.path.exists(cf):
+      self.conf.read([cf])
+    elif not force:
+      raise Exception("Configuration file missing")
+    
+    if not force:
+      vers = int(self.conf.get("core", "repositoryformatversion"))
+      if vers != 0 and not force:
+        raise Exception("Unsupported repositoryformatversion %s" % vers)
+
+def repoPath(repo, *path):
+  """Compute path under repo's gitdir."""
+  return os.path.join(repo.gitdir, *path)
+
+def repoFile(repo, *path, mkdir=False):
+  """Same as repoPath, but create dirname(*path) if absent."""
+
+  if repoDir(repo, *path[:-1], mkdir=mkdir):
+    return repoPath(repo, *path)
+
+def repoDir(repo, *path, mkdir=False):
+  """Same as repoPath, but mkdir *path if absent if mkdir."""
+
+  path = repoPath(repo, *path)
+
+  if os.path.exists(path):
+    if(os.path.isdir(path)):
+      return path
+    else:
+      raise Exception("Not a directory %s" % path)
+
+  if mkdir:
+    os.makedirs(path)
+    return path
+  else:
+    return None
+  
+def repoCreate(path):
+  """Create a new repository at path."""
+
+  repo = verFlowRepository(path, True)
+
+  # First we make sure the path either doesn't exist or is an
+  # empty dir
+
+  if os.path.exists(repo.worktree):
+    if not os.path.isdir(repo.worktree):
+      raise Exception ("%s is not a directory!" % path)
+    if os.listdir(repo.worktree):
+      raise Exception("%s is not empty!" % path)
+
+  else:
+    os.makedirs(repo.worktree)
+
+  assert(repoDir(repo, "branches", mkdir=True))
+  assert(repoDir(repo, "objects", mkdir=True))
+  assert(repoDir(repo, "refs", "tags", mkdir=True))
+  assert(repoDir(repo, "refs", "heads", mkdir=True))
+
+  # .git/description
+  with open(repoFile(repo, "description"), "w") as f:
+    f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+  
+  # .git/HEAD
+  with open(repoFile(repo, "HEAD"), "w") as f:
+    f.write("ref: refs/heads/master\n")
+
+  with open(repoFile(repo, "config"), "w") as f:
+    config = repoDefaultConfig()
+    config.write(f)
+
+  return repo
+  
+def repoDefaultConfig():
+  defConfig = configparser.ConfigParser()
+
+  defConfig.add_section("core")
+  defConfig.set("core", "repositoryformatversion", "0")
+  defConfig.set("core", "filemode", "false")
+  defConfig.set("core", "bare", "false")
+
+  return defConfig
